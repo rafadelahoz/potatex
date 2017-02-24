fd7ad322c6b45a10fbeb8085999a1bd58dbde29b
Rafa de la Hoz <rafa.delahoz@gmail.com>
2017-02-24 12:53:34 +0100

    Testing popups and things

--- source/Main.hx
+++ source/Main.hx
@@ -5,12 +5,21 @@ import haxe.ui.toolkit.core.Toolkit;
 import haxe.ui.toolkit.core.Root;
 import haxe.ui.toolkit.controls.Button;
 import haxe.ui.toolkit.events.UIEvent;
+import haxe.ui.toolkit.themes.GradientTheme;
+import haxe.ui.toolkit.core.interfaces.IDisplayObject;
+import haxe.ui.toolkit.core.PopupManager;
 
 class Main {
     public static function main() {
-        // Macros.addStyleSheet("styles/gradient/gradient.css");
+        Toolkit.theme = new GradientTheme();
         Toolkit.init();
         Toolkit.openFullscreen(function(root:Root) {
+
+            var view : IDisplayObject = Toolkit.processXmlResource("layouts/layout.xml");
+            root.addChild(view);
+
+            PopupManager.instance.showBusy("Please wait", 1000, "Busy popup");
+
             var button:Button = new Button();
             button.text = "Click Me!";
             button.x = 100;
