7b11d01530b0d8ed4f80736f2f1927499f7c47d6
Rafa de la Hoz <rafa.delahoz@gmail.com>
2017-03-03 11:51:56 +0100

    Added styling to already answered questions

--- source/test/TestController.hx
+++ source/test/TestController.hx
@@ -34,20 +34,21 @@ class TestController extends XMLController
 
     function fetchQuestion(?Next : Int = -1)
     {
-        if (Next <= 0)
-            Next = 1;
+        if (Next < 0)
+            Next = 0;
 
-        if (Next > 0 && data.questions.length > Next)
+        // Cycle!
+        if (Next >= data.questions.length)
+            Next = 0;
+
+        if (Next > -1 && Next < data.questions.length)
         {
             currentQuestionIndex = Next;
 
-            // Don't fail on next for now
             currentQuestion = data.questions[Next];
 
-            trace("Q"+Next+": " + currentQuestion);
-
             var text : Text = getComponentAs("txtQuestion", Text);
-            text.text = currentQuestion.question.text;
+            text.text = (Next+1) + "-" + currentQuestion.question.text;
             text.multiline = true;
             text.wrapLines = true;
 
@@ -58,16 +59,81 @@ class TestController extends XMLController
                 text.text = answer;
                 text.wrapLines = true;
                 text.multiline = true;
+                resetAnswerStyle(text);
 
                 // Check correctness
                 // if (index == currentQuestion.question.correct)
+                switch (currentQuestion.state)
+                {
+                    case Pending:
+                        styleAnswerPending(text);
+                    case Answered:
+                        styleAnswerAnswered(index, text);
+                    case Aftermath:
+                        styleAnswerAftermath(index, text);
+
+                }
 
                 index += 1;
+            }
+
+            // Handle question state
+            switch (currentQuestion.state)
+            {
+                case Pending:
+                    setAnswerButtonsElabled(true);
+                case Answered:
+                    setAnswerButtonsElabled(true);
+                case Aftermath:
+                    setAnswerButtonsElabled(false);
             }
         }
         else
         {
             throw "Question not found " + Next;
+        }
+    }
+
+    function styleAnswerPending(text : Text)
+    {
+        // NoP!
+    }
+
+    function styleAnswerAnswered(index : Int, text : Text)
+    {
+        if (currentQuestion.answer == index)
+        {
+            text.style.fontBold = true;
+        }
+    }
+
+    function styleAnswerAftermath(index : Int, text : Text)
+    {
+        if (index == currentQuestion.question.correct)
+        {
+            text.style.backgroundColor = 0x00FF0a;
+            text.style.backgroundAlpha = 1;
+        }
+        else if (currentQuestion.answer == index)
+        {
+            text.style.backgroundColor = 0xFF000a;
+            text.style.backgroundAlpha = 1;
+        }
+    }
+
+    function resetAnswerStyle(text : Text)
+    {
+        text.style.backgroundColor = 0xFFFFFF;
+        text.style.backgroundAlpha = 0;
+        text.style.fontBold = false;
+    }
+
+    function setAnswerButtonsElabled(enabled : Bool)
+    {
+        for (id in 1...5)
+        {
+            var btnId = "btnA" + id;
+            getComponentAs(btnId, Button).disabled = !enabled;
         }
     }
 
